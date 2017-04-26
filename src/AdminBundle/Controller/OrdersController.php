ebb456f69e1b2b6994e428e2f610fa55e9f6bfd9
Lakov <mlakov@abv.bg>
2017-04-26 17:23:08 +0300

    admin view single order and its products

---
+++ src/AdminBundle/Controller/OrdersController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use ShopBundle\Entity\Product;
+use ShopBundle\Entity\Category;
+use \ShopBundle\Entity\CustomerOrder;
+use \ShopBundle\Entity\OrderProduct;
+class OrdersController extends Controller
+{
+    /**
+     * @Route("/admin/orders", name="admin-orders-show")
+     * 
+     */
+    public function listOrdersAction(Request $request)
+    {
+        $orders = $this->getDoctrine()->getRepository(CustomerOrder::class)->findAll();
+        return $this->render('admin/manage_orders.html.twig',array(
+            'orders' => $orders              
+        ));
+               
+    }
+     /**
+     * @Route("/admin/orders/single/{orderId}", name="admin-orders-show-single")
+     * @param $orderId int
+     */
+    public function showSingleOrderAction(Request $request,$orderId)
+    {
+       
+        $order = $this->getDoctrine()->getRepository(CustomerOrder::class)->find($orderId);
+        $orderProducts = $this->getDoctrine()->getRepository(OrderProduct::class)->findBy(array('order'=>$order));
+        return $this->render('admin/orders/order.html.twig',array(
+            'order' => $order,
+            'orderProducts' =>$orderProducts
+        ));
+               
+    }
+
+
+}
