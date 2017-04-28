8c15ac691a25eaf145cc6c9b05651d509449e835
Lakov <mlakov@abv.bg>
2017-04-28 09:53:19 +0300

    latest products service

---
+++ src/ShopBundle/Service/ProductModules.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ShopBundle\Service;
+use Doctrine\ORM\EntityManager;
+use ShopBundle\Entity\Product;
+class ProductModules {
+    
+    private $entityManager;
+    
+    public function __construct(EntityManager $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+    public function latestProductsAction($limit = 5){
+        $repo = $this->entityManager->getRepository(Product::class);
+        $result = $repo->findBy(array(),array('id' => 'ASC'),$limit);
+        return $result;
+    }
+       
+}
